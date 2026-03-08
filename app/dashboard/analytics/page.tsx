79fccded0fc9b11b9d85617610d9cee1020666fc
pekopekp <sanaba771@gmail.com>
2026-03-08 23:50:19 +0530

    Dynamic routes

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -3,13 +3,7 @@ import React from 'react'
 const Analytics = () => {
   return (
     <div>
-        <h1>Dashbaord Users</h1>
-        <ul className="mt-10">
-            <li>User 1</li>
-            <li>User 2</li>
-            <li>User 3</li>
-            <li>User 4</li>
-        </ul>
+        
     </div>
   )
 }
