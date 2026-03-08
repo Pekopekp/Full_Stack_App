6a59ac32964a19a8183908d2dd0ba24cc42fb2ea
pekopekp <sanaba771@gmail.com>
2026-03-09 00:12:24 +0530

    Dynamic routes

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 
 ## Getting Started
 
+# Environment
+
+npm install
+
+npm install babel-plugin-react-compiler@latest
+
 First, run the development server:
 
 ```bash
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        Coming from the main layout
+        Navbar
         {children}
+        Footer 
       </body>
     </html>
   );
