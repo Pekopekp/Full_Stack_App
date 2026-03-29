c244c5b35a0f166bac600de38db21cbda373afe5
pekopekp <sanaba771@gmail.com>
2026-03-29 23:48:41 +0530

    Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
 
 const page = () => {
@@ -5,6 +6,7 @@ const page = () => {
     <section>
       <h1 className="text-center"> The Hub for Every Dev <br/> Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conference, All in one place</p>
+      <ExploreBtn/>
     </section>
   )
 }
